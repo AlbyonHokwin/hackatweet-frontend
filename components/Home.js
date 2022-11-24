f89ae42ada2becacb958b61ba039de3801359e01
Camille HAUSTANT <camillehaustant@gmail.com>
2022-11-24 17:41:28 +0100

    left section done - middle section in progress

--- components/Home.js
+++ components/Home.js
@@ -1,33 +1,35 @@
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 function Home() {
   return (
-
-    <div className={styles.Home} >
-
-      <div className={styles.colonneleft} >
-        <div className={styles.logo}>
-          <img src="twitter.png" alt="Logo" />
+    <div className={styles.container}>
+      
+      <div className={styles.leftSection} >
+        <Link href="/" ><Image className={styles.logo} src="/twitter.png" alt="Logo" width={60} height={60} /></Link>
+        <div className={styles.userSection}>
+          <div className={styles.user}>
+            <Image className={styles.userImg} src="/twitter.png" alt="User image" width={60} height={60} />
+            <div className={styles.userInfos}>
+              <div className={styles.firstname} >FirstName</div>
+              <div className={styles.username} > UserName</div>
+            </div>
+          </div>
+          <button className={styles.btnLogout}>Logout</button>
         </div>
       </div>
 
-      <div className={styles.colonneright} >
-        <div className={styles.petitlogo}>
-          <img src="twitter.png" alt="petitlogo" />
-          </div>
-          <div className={styles.rangement}>
-          <h1 className={styles.title}>SEE WHAT'S</h1>
-          <h1 className={styles.title}>HAPPENING</h1>
-          </div>
-          <h2 className={styles.soustitre}>JOIN HACKATWEET TODAY.</h2>
-          <button className={styles.buttoncompact}>Sign Up</button>
-          <p className={styles.p}>Already have an account</p>
-          <button className={styles.buttonbordure}>Sign in</button>
-
-
-        
+      <div className={styles.middleSection} >
+        <div className={styles.addSection}>
+          <h1>Home</h1>
+          <input className={styles.inputAdd} placeholder="What's up" />
+          <div className={styles.countAndAdd}>Count & ADD</div>
+        </div>
+        <div className={styles.tweetsContainer}>TWEETS</div>
       </div>
 
+      <div className={styles.rightSection} >RIGHT</div>
     </div>
   );
 }
