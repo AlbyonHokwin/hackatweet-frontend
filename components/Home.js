5abc3bf91f8c45affc81d8161ee4c359a200d88d
Juliette <juliette.durel@outlook.fr>
2022-11-24 15:39:42 +0100

    Home-LOGIN

--- components/Home.js
+++ components/Home.js
@@ -2,12 +2,32 @@ import styles from '../styles/Home.module.css';
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+
+    <div className={styles.Home} >
+
+      <div className={styles.colonneleft} >
+        <div className={styles.logo}>
+          <img src="twitter.png" alt="Logo" />
+        </div>
+      </div>
+
+      <div className={styles.colonneright} >
+        <div className={styles.petitlogo}>
+          <img src="twitter.png" alt="petitlogo" />
+          </div>
+          <div className={styles.rangement}>
+          <h1 className={styles.title}>SEE WHAT'S</h1>
+          <h1 className={styles.title}>HAPPENING</h1>
+          </div>
+          <h2 className={styles.soustitre}>JOIN HACKATWEET TODAY.</h2>
+          <button className={styles.buttoncompact}>Sign Up</button>
+          <p className={styles.p}>Already have an account</p>
+          <button className={styles.buttonbordure}>Sign in</button>
+
+
+        
+      </div>
+
     </div>
   );
 }
---
+++ components/Login.js
@@ -0,0 +1,34 @@
+import styles from '../styles/Login.module.css';
+
+function Login() {
+    return (
+
+        <div className={styles.Home} >
+    
+          <div className={styles.colonneleft} >
+            <div className={styles.logo}>
+              <img src="twitter.png" alt="Logo" />
+            </div>
+          </div>
+    
+          <div className={styles.colonneright} >
+            <div className={styles.petitlogo}>
+              <img src="twitter.png" alt="petitlogo" />
+              </div>
+              <div className={styles.rangement}>
+              <h1 className={styles.title}>SEE WHAT'S</h1>
+              <h1 className={styles.title}>HAPPENING</h1>
+              </div>
+              <h2 className={styles.soustitre}>JOIN HACKATWEET TODAY.</h2>
+              <button className={styles.buttoncompact}>Sign Up</button>
+              <p className={styles.p}>Already have an account</p>
+              <button className={styles.buttonbordure}>Sign in</button>
+    
+    
+            
+          </div>
+    
+        </div>
+      );}
+
+export default Login;
