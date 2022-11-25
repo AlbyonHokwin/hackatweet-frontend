7ca68d4d88752c5d288fe57ebc8d26d5f066c246
Camille HAUSTANT <camillehaustant@gmail.com>
2022-11-25 16:33:18 +0100

    bugs correction of redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css';
 import Head from 'next/head';
-import { configureStore } from '@reduxjs/toolkit';
-import tweets from './reducers/tweets';
 import { Provider } from 'react-redux';
-import user from './reducers/user';
-import likes from "./reducers/likes";
+import { configureStore } from '@reduxjs/toolkit';
+import tweets from '../reducers/tweets';
+import user from '../reducers/user';
+import likes from "../reducers/likes";
 
-const store = configureStore ({reducers:{tweets,user,likes}})
+const store = configureStore({
+  reducer: { tweets, user, likes }
+});
 
 function App({ Component, pageProps }) {
   return (
--- reducers/tweets.js
+++ reducers/tweets.js
@@ -1,24 +1,22 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-   value : []
-
+    value: []
 }
 
-export const tweetsSlice = createSlice ({
- name: "tweets", 
- initialState, 
- reducers: {
-    addtweet: (state, action) => {
-        state.value.push(action.payload)
-    }, 
-    removetweet: (state, action) => {
-        state.value = state.value.filter(like => like.message !==action.payload.message && like.date !==action.payload.date)
-        console.log (action.payload )
-    }, 
-
- }
+export const tweetsSlice = createSlice({
+    name: "tweets",
+    initialState,
+    reducers: {
+        addtweet: (state, action) => {
+            state.value.push(action.payload)
+        },
+        removetweet: (state, action) => {
+            state.value = state.value.filter(like => like.message !== action.payload.message && like.date !== action.payload.date)
+            console.log(action.payload)
+        },
+    }
 })
 
-export const { addtweet, removetweet} = tweetSlice.actions; 
+export const { addtweet, removetweet } = tweetsSlice.actions;
 export default tweetsSlice.reducer
